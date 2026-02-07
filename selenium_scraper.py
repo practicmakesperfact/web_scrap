da6e2df62efc78e8695fc51c9dfcac20119ec07c
Haymanot Asmare <hasmare463@gmail.com>
2026-02-07 00:46:40 -0800

    Refactor test_selenium.py for improved structure and add functionality to scrape quotes; create selenium_scraper.py for data extraction and CSV export

---
+++ selenium_scraper.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+import csv
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://quotes.toscrape.com/js/")
+
+time.sleep(3)
+
+data = []
+
+while True:
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    quotes = soup.find_all("div", class_="quote")
+    for q in quotes:
+        text = q.find("span", class_="text").get_text(strip=True)
+        author = q.find("small", class_="author").get_text(strip=True)
+
+        data.append({
+            "quote": text,
+            "author": author
+        })
+
+    try:
+        next_btn = driver.find_element("css selector", "li.next a")
+        next_btn.click()
+        time.sleep(2)
+    except:
+        break
+
+driver.quit()
+
+with open("selenium_quotes.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=["quote", "author"])
+    writer.writeheader()
+    writer.writerows(data)
+
+print("Saved selenium_quotes.csv")
--- test_selenium.py
+++ test_selenium.py
@@ -1,10 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://quotes.toscrape.com")
+options = Options()
+options.add_argument("--start-maximized")
 
-print(driver.title)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 
+driver.get("https://quotes.toscrape.com/js/")
+time.sleep(3)
+
+while True:
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    quotes = soup.find_all("div", class_="quote")
+    for q in quotes:
+        print(q.find("small", class_="author").text)
+
+    try:
+        next_btn = driver.find_element(By.CSS_SELECTOR, "li.next a")
+        next_btn.click()
+        time.sleep(2)
+    except:
+        print("No more pages")
+        break
+
+input("Press ENTER to close browser...")
 driver.quit()
